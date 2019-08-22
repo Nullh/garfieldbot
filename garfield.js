cb8cae26d9395e61ba0f1d85bd68c1cc76c3c3bb
Neil Holmes <Neil.Holmes2@jet2.com>
2019-08-22 08:53:56 +0100

    Now sends a comic

--- garfield.js
+++ garfield.js
@@ -1,15 +1,27 @@
 const Discord = require("discord.js");
 require('dotenv').config();
 const client = new Discord.Client();
+const prefix = process.env.PREFIX;
  
 client.on("ready", () => {
   console.log("I am ready!");
 });
  
 client.on("message", (message) => {
+
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+
+  if (command === 'comic') {
+    message.channel.send('https://garfield.com/comic/random')
+  } else
   if (message.content.startsWith("!garf")) {
-    message.channel.send("Yes, Jon?");
+    message.channel.send("Yes, Jon?\nAsk me for a comic by typing !garf comic");
   }
+
+
 });
  
 client.login();
