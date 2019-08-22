0cd95908a12d327c70124976827fcb37fab4273e
Neil Holmes <Neil.Holmes2@jet2.com>
2019-08-22 16:53:48 +0100

    Unified all responses into one file __RANDEMOJI__ will now be replaced with a random, valid emoji. Took out the specific bit of code for garf and garfield responses

--- events/message.js
+++ events/message.js
@@ -1,30 +1,28 @@
-//client.on("message", (message) => {
 module.exports = (client, message) => {
 
     if (message.author.bot) return;
 
-    
-    if (!message.content.startsWith(client.prefix)) {
+    // Replace the string __RANDEMOJI__ with a random emoji
+    function replaceEmoji(string) {
+        var rand = Math.floor(Math.random() * (client.validEmojis['emojis'].length));
+        var myEmoji = client.emojis.find(emoji => emoji.name === client.validEmojis['emojis'][rand]);
+        string = string.replace("__RANDEMOJI__", myEmoji);
+        return string;
+    }
 
-        // Respond to garf or garfield
-        if (message.content.toLowerCase().includes('garf') || message.content.toLowerCase().includes('garfield')) {
-            var rand = Math.floor(Math.random() * (client.validEmojis['emojis'].length + 1))
-            //var myEmoji = client.emojis.random();
-            var myEmoji = client.emojis.find(emoji => emoji.name === client.validEmojis['emojis'][rand]);
-            message.channel.send(`${myEmoji} `)
-            return;
-        }
+    if (!message.content.startsWith(client.prefix)) {
         // Respond to any words in the responses.json
         var triggerWords = Object.keys(client.responses);
         for(key in triggerWords) {
             if(message.content.toLowerCase().includes(triggerWords[key])) {
-                console.log(`Value is ${client.responses[triggerWords[key]]}`)
-                if(typeof client.responses[triggerWords[key]] == 'string'){
-                    message.channel.send(`${client.responses[triggerWords[key]]}`)
+                if(typeof client.responses[triggerWords[key]] == 'string') {
+                    message.channel.send(`${replaceEmoji(client.responses[triggerWords[key]])}`);
+                    return;
                 } else
-                if(typeof client.responses[triggerWords[key]] == 'object'){
-                    var rand = Math.floor(Math.random() * (client.responses[triggerWords[key]].length + 1))
-                    message.channel.send(`${client.responses[triggerWords[key]][rand]}`)
+                if(typeof client.responses[triggerWords[key]] == 'object') {
+                    var rand = Math.floor(Math.random() * (client.responses[triggerWords[key]].length + 1));
+                    message.channel.send(`${replaceEmoji(client.responses[triggerWords[key]][rand])}`);
+                    return;
                 }
             }
         }
@@ -34,7 +32,7 @@ module.exports = (client, message) => {
 
     const args = message.content.slice(client.prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-  
+
     if (command === 'garf') {
       let [subcommand] = args
       switch (subcommand){
