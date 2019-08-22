80964414926bbf1a1ca3e99aca72553ab76e3449
Neil Holmes <Neil.Holmes2@jet2.com>
2019-08-22 09:54:07 +0100

    Added emoji power!

--- garfield.js
+++ garfield.js
@@ -11,9 +11,10 @@ client.on("message", (message) => {
 
   if (message.author.bot) return;
 
-  if (message.content.includes('garf') || message.content.includes('garfield') && message.content.startsWith(prefix)) {
-    message.channel.send('You called?')
-    return;
+  if (!message.content.startsWith(prefix) && (message.content.includes('garf') || message.content.includes('garfield'))) {
+    const gainsfield = client.emojis.find(emoji => emoji.name === "gainsfield");
+    //message.reply(`You called ${gainsfield}`)
+    message.channel.send(`You called ${gainsfield}`)
   }
 
   //Start of prefix only commands
@@ -22,15 +23,16 @@ client.on("message", (message) => {
   const args = message.content.slice(prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
 
-  console.log(`Commands are ${command}`)
+  console.log(`Commands are ${command} args are ${args}`)
 
   if (command === 'garf') {
     let [subcommand] = args
     if (subcommand == null) {
       message.channel.send('Yes, Jon?\nAsk me for a comic by typing !garf comic\nGet a list of emojis I can access with listemojis')
     } else
-    if (subcommand === ' listemojis') {
-      const emojiList = message.guild.emojis.map((e, x) => (x + ' = ' + e) + ' | ' +e.name).join('\n');
+    if (subcommand === 'listemojis') {
+      console.log(`Listing emojis`)
+      const emojiList = message.guild.emojis.map(e=>e.toString()).join(" ");
       message.channel.send(emojiList);
     } else
     if (subcommand === 'comic'){
