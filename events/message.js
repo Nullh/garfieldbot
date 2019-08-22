df28d034bf5348e64ad06e436d99daccd828f269
Neil Holmes <Neil.Holmes2@jet2.com>
2019-08-22 10:33:55 +0100

    Tried moving trigger words to json

--- events/message.js
+++ events/message.js
@@ -2,15 +2,26 @@
 module.exports = (client, message) => {
 
     if (message.author.bot) return;
+
+    var responsefile = fs.readFileSync("response.json")
+    var response = JSON.parse(responsefile)
   
+    /*
     if (!message.content.startsWith(prefix) && (message.content.includes('garf') || message.content.includes('garfield'))) {
       const gainsfield = client.emojis.find(emoji => emoji.name === "gainsfield");
       //message.reply(`You called ${gainsfield}`)
-      message.channel.send(`You called ${gainsfield}`)
+      message.channel.send(`You called? ${gainsfield}`)
     }
+    */
+
   
     //Start of prefix only commands
-    if (!message.content.startsWith(prefix)) return;
+    if (!message.content.startsWith(prefix)) {
+        if(response[message.content]) {
+            message.channel.send(response[message.content])
+        }
+        return;
+    }
   
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
